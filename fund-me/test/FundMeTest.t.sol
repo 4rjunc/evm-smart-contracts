7ba56771ad74a2e6325442adafbe204937de4277
4rjunc <arjunmundamani@gmail.com>
2025-10-20 16:41:59 +0530

    deploy

--- fund-me/test/FundMeTest.t.sol
+++ fund-me/test/FundMeTest.t.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract FundMe
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    uint256 number = 1;
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+        console.log("Hello");
+        number = 2;
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        address owner = fundMe.i_owner();
+        assertEq(owner, address(this));
+    }
+}
