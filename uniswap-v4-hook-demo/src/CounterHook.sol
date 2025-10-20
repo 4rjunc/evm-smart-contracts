c27b7a5661da5166744d3e804f12186157468050
4rjunc <arjunmundamani@gmail.com>
2025-10-20 10:15:49 +0530

    hooks

--- uniswap-v4-hook-demo/src/CounterHook.sol
+++ uniswap-v4-hook-demo/src/CounterHook.sol
@@ -6,50 +6,64 @@ import "@uniswap/src/base/BaseHook.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "@uniswap/lib/v4-core/src/types/BeforeSwapDelta.sol";
 import {IPoolManager, SwapParams, ModifyLiquidityParams} from "@uniswap/lib/v4-core/src/interfaces/IPoolManager.sol";
 
-contract CounterHook is BaseHook{
-
+contract CounterHook is BaseHook {
     uint24 public swapNumber = 1;
     error MaxNumberReached();
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: false,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: true,
-            afterSwap: true,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
     }
 
     function beforeSwap(
-      address,
-      PoolKey calldata,
-      SwapParams calldata,
-      bytes calldata
-    ) external override onlyPoolManager returns (bytes4, BeforeSwapDelta, uint24) {
-      if (swapNumber >= 10) revert MaxNumberReached();
-      return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        address,
+        PoolKey calldata,
+        SwapParams calldata,
+        bytes calldata
+    )
+        external
+        override
+        onlyPoolManager
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        if (swapNumber >= 10) revert MaxNumberReached();
+        return (
+            BaseHook.beforeSwap.selector,
+            BeforeSwapDeltaLibrary.ZERO_DELTA,
+            0
+        );
     }
 
     function afterSwap(
-      address,
-      PoolKey calldata,
-      SwapParams calldata,
-      BalanceDelta,
-      bytes calldata
+        address,
+        PoolKey calldata,
+        SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
     ) external override onlyPoolManager returns (bytes4, int128) {
-      swapNumber++;
-      return (BaseHook.afterSwap.selector, 0);
+        swapNumber++;
+        return (BaseHook.afterSwap.selector, 0);
     }
- }
+}
